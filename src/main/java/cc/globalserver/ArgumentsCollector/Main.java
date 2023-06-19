532833172c0dd84fe52172eeff8b3e090d450d18
Learting <learting@hotmail.com>
2023-06-19 21:06:00 +0200

    基础框架

---
+++ src/main/java/cc/globalserver/ArgumentsCollector/Main.java
@@ -0,0 +1,124 @@
+package cc.globalserver.ArgumentsCollector;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.ChatColor;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+import java.util.Map;
+
+public class Main extends JavaPlugin implements CommandExecutor, Listener, TabCompleter {
+
+    private final HashMap<UUID, List<String>> playerInputs = new HashMap<>();
+    private final HashMap<UUID, String[]> messagesQueue = new HashMap<>();
+    private final HashMap<UUID, String> commandQueue = new HashMap<>();
+
+    @Override
+    public void onEnable() {
+        this.getCommand("ac").setExecutor(this);
+        this.getCommand("ac").setTabCompleter(this);
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player && command.getName().equalsIgnoreCase("ac") && args.length >= 2) {
+            Player player = (Player) sender;
+            UUID playerId = player.getUniqueId();
+            
+            String cmd = args[0];
+            String[] messages = new String[args.length - 1];
+            System.arraycopy(args, 1, messages, 0, args.length - 1);
+
+            playerInputs.put(playerId, new ArrayList<>());
+            messagesQueue.put(playerId, messages);
+            commandQueue.put(playerId, cmd);
+            
+            player.sendMessage(ChatColor.LIGHT_PURPLE + "⎡" + ChatColor.WHITE + messages[0]);
+            return true;
+        }
+        return false;
+    }
+
+    @EventHandler
+    public void onPlayerChat(AsyncPlayerChatEvent event) {
+        Player player = event.getPlayer();
+        UUID playerId = player.getUniqueId();
+
+        if (playerInputs.containsKey(playerId)) {
+            List<String> inputs = playerInputs.get(playerId);
+            String[] messages = messagesQueue.get(playerId);
+            String cmd = commandQueue.get(playerId);
+
+            String message = event.getMessage();
+
+            if ("q".equalsIgnoreCase(message)) {
+                playerInputs.remove(playerId);
+                messagesQueue.remove(playerId);
+                commandQueue.remove(playerId);
+                player.sendMessage(ChatColor.DARK_RED + "已取消执行");
+                event.setCancelled(true);
+                return;
+            }
+
+            inputs.add(message);
+            player.sendMessage(ChatColor.LIGHT_PURPLE + "⎣" + ChatColor.AQUA + message);
+
+            if (inputs.size() < messages.length) {
+                player.sendMessage(ChatColor.LIGHT_PURPLE + "⎡" + ChatColor.WHITE + messages[inputs.size()]);
+                event.setCancelled(true);
+                return;
+            }
+
+            StringBuilder commandToExecute = new StringBuilder(cmd);
+            for (String input : inputs) {
+                commandToExecute.append(" ").append(input);
+            }
+
+            Bukkit.getScheduler().runTask(this, () -> player.performCommand(commandToExecute.toString()));
+
+            playerInputs.remove(playerId);
+            messagesQueue.remove(playerId);
+            commandQueue.remove(playerId);
+            event.setCancelled(true);
+        }
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        List<String> tabCompleteList = new ArrayList<>();
+        if (sender instanceof Player) {
+            if (args.length == 1) {
+                for (Command cmd : Bukkit.getCommandMap().getKnownCommands().values()) {
+                    String cmdName = cmd.getName().toLowerCase();
+                    if (cmdName.startsWith(args[0].toLowerCase())) {
+                        tabCompleteList.add(cmdName);
+                    }
+                }
+            } else if (args.length > 1) {
+                Command targetCommand = Bukkit.getCommandMap().getKnownCommands().get(args[0]);
+                if (targetCommand != null) {
+                    String[] targetArgs = Arrays.copyOfRange(args, 1, args.length);
+                    List<String> targetSuggestions = targetCommand.tabComplete(sender, alias, targetArgs);
+                    if (targetSuggestions != null) {
+                        tabCompleteList.addAll(targetSuggestions);
+                    }
+                }
+            }
+        }
+        return tabCompleteList;
+    }
+}
